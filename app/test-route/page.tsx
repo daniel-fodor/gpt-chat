dd3136d4b7366c7c385c6d47e25ab2026dd6c4c4
Daniel Fodor <fodordaniel89@gmail.com>
2026-09-19 13:57:51 +0200

    layout

---
+++ app/test-route/page.tsx
@@ -0,0 +1,8 @@
+export default function TestRoute() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Test Route</h1>
+      <div className="mt-5">This is just a test route, you should delete me.</div>
+    </main>
+  );
+}
