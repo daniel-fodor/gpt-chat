80993532f0f1b9980d0fd0e9e730c1123437b2ee
Daniel Fodor <fodordaniel89@gmail.com>
2026-09-19 22:07:21 +0200

    chat gpt

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
-import ServerComponent from "./ServerComponent"
-import ClientComponent from "./ClientComponent"
+"use client"
+import Chat from "@/components/Chat"
+import { Separator } from "@/components/ui/separator"
+import { useSession } from "next-auth/react"
 
-export default async function Home() {
-  const res = await fetch("https://reqres.in/api/users/2")
-  const { data } = await res.json()
+export default function Home() {
+  const { data: session, status } = useSession()
 
   return (
-    <main className="mt-5 max-w-xl mx-auto">
-      <ServerComponent />
-      <ClientComponent name={`${data.first_name} ${data.last_name}`} />
+    <main className="mx-auto max-w-xl">
+      <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
+      {!session?.user?.email && (
+        <p className="my-5">
+          You are not signed in. Please sign in to continue.
+        </p>
+      )}
+      {session?.user?.email && (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   )
 }
