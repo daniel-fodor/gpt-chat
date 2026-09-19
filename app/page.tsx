4f99e9cae547b6574d112c94b8cf58663985d975
Daniel Fodor <fodordaniel89@gmail.com>
2026-09-19 14:10:31 +0200

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import ServerComponent from "./ServerComponent"
+import ClientComponent from "./ClientComponent"
 
 export default function Home() {
   return (
-      <main className="p-5">
-        <h1 className="text-4xl font-bold">Welcome to GPT chat</h1>
-      </main>
-  );
+    <main className="mt-5 max-w-xl mx-auto">
+      <ServerComponent />
+      <ClientComponent />
+    </main>
+  )
 }
