542e8cd056b7b6d46ee83a8f13d7123f37363722
Daniel Fodor <fodordaniel89@gmail.com>
2026-09-20 20:38:31 +0200

    store cha

---
+++ db/index.ts
@@ -0,0 +1,9 @@
+import type { Chat, ChatWithMessages, Message } from "../types/types"
+
+export async function createChat() {}
+
+export async function getChat() {}
+
+export async function getChats() {}
+
+export async function getMessages(chatId: number) {}
